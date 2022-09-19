2bfa9d6885f787b4217b7d487503d1020f2da285
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-09-19 11:05:58 +0200

    Add files via upload

---
+++ messageBox/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace messageBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxRezultat = new System.Windows.Forms.TextBox();
+            this.buttonOtvori = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxRezultat
+            // 
+            this.txtBoxRezultat.Location = new System.Drawing.Point(60, 120);
+            this.txtBoxRezultat.Multiline = true;
+            this.txtBoxRezultat.Name = "txtBoxRezultat";
+            this.txtBoxRezultat.Size = new System.Drawing.Size(187, 130);
+            this.txtBoxRezultat.TabIndex = 0;
+            // 
+            // buttonOtvori
+            // 
+            this.buttonOtvori.Location = new System.Drawing.Point(60, 64);
+            this.buttonOtvori.Name = "buttonOtvori";
+            this.buttonOtvori.Size = new System.Drawing.Size(187, 23);
+            this.buttonOtvori.TabIndex = 1;
+            this.buttonOtvori.Text = "Otvori Message box";
+            this.buttonOtvori.UseVisualStyleBackColor = true;
+            this.buttonOtvori.Click += new System.EventHandler(this.mbButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 331);
+            this.Controls.Add(this.buttonOtvori);
+            this.Controls.Add(this.txtBoxRezultat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxRezultat;
+        private System.Windows.Forms.Button buttonOtvori;
+    }
+}
+
---
+++ messageBox/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace messageBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mbButton_Click(object sender, EventArgs e)
+        {
+            DialogResult rez= MessageBox.Show("Odaberite Da/Ne/Odustani", "Naslovna linija Dijaloga za poruke", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+            
+            
+            switch (rez)
+
+            {
+
+                case DialogResult.Yes:
+
+                    txtBoxRezultat.Text = "Izabrali ste dugme DA";
+
+                    break;
+
+                case DialogResult.No:
+
+                    txtBoxRezultat.Text = "Izabrali ste dugme NE";
+
+                    break;
+
+                case DialogResult.Cancel:
+
+                    txtBoxRezultat.Text = "Izabrali ste dugme ODUSTANI";
+
+                    break;
+
+            }
+        }
+    }
+}
